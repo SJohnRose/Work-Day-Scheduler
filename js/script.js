edc067c7128be5e56945a913a8c9f215a42165f2
SJohnRose <shamini.johnrose@gmail.com>
2023-01-28 22:03:56 +1100

    added function to display current date

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+// Reference to DOM elements
 var saveButtonEl = $('.saveBtn');
-var timeNow = dayjs().format('h');
-console.log(timeNow);
+var currentDayEl = $('#currentDay');
 
 $(function () {
+  
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -24,4 +26,10 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  function displayDate() {
+    var timeNow = dayjs().format('dddd, MMMM DD[th]');
+    console.log(timeNow);
+    currentDayEl.text(timeNow);
+  }
+  displayDate();
 });
